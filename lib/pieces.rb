c96d6f31525ae59e8267ad180dd3c43ea59df754
James Hopkins <i.james.hopkins@gmail.com>
2020-05-27 14:26:34 -0400

    added knight piece

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,5 +1,42 @@
 class Piece
+    attr_accessor :position, :colour, :path, :moves
+    def initialize(start = [0,0], colour="black", path =[])
+        @colour = colour
+        @path = path[0..-1]
+        @path.push(start)
+        @position = start
+        @moves = []
+    end
+
+    def move(move_pos, ally_positions)
+        if self.valid_move?(move_pos) && !ally_positions.include?(move_pos)
+            self.position = move_pos 
+            return true
+        else
+            return false
+        end
+
+    end
+
+    def valid_move?(move)
+        self.submoves
+        if self.moves.include?(move)
+            return true
+        else
+            return false
+        end
+    end
 end
 
-class Knight > Piece
+class Knight < Piece
+    MOVES = [[1,2], [2,1], [1,-2], [2,-1], [-1,2], [-2,1], [-1, -2], [-2,-1]]
+
+    def submoves
+        MOVES.each do |cords|
+            new_cords = [cords[0] + @position[0], cords[1] + @position[1]]
+            if new_cords[0] <= 8 && new_cords[0] >= 0 && new_cords[1] <= 8 && new_cords[1] >= 0 
+                @moves.push(new_cords)
+            end
+        end
+    end
 end
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -3,8 +3,8 @@ require './lib/pieces'
 describe Knight do
     describe "#move" do
         it "moves the piece to x,y" do
-            knight = Knight.new(0,0, "black")
-            knight.move(1,2)
+            knight = Knight.new([0,0], "black")
+            knight.move([1,2], [])
             expect(knight.position).to eql([1,2])
         end
     end
