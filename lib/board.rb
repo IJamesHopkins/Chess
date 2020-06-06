c2cc9a845c1f96b6d69d2e891c2733197fe92721
James Hopkins <i.james.hopkins@gmail.com>
2020-06-06 14:28:48 -0400

    added visual board

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+require './lib/pieces.rb'
+
 class Board
 
     def initialize
@@ -24,4 +26,36 @@ class Board
         pieces.push(King.new([4,piece_location[1]],colour,[]))
         return pieces
     end
+
+    def place_board
+        8.times do |x|
+            8.times do |y|
+                piece_to_put = piece_at_location([y,x])
+                print "| #{piece_to_put} "
+            end
+            print "| \n"
+            8.times do
+                print "----"
+            end
+            print "-\n"
+        end
+    end
+
+    def piece_at_location(loc)
+        @black_pieces.each do |piece|
+            if piece.position == loc
+                return piece.piece_token
+            end
+        end
+
+        @white_pieces.each do |piece|
+            if piece.position == loc
+                return piece.piece_token
+            end
+        end
+        return " "
+    end
 end
+
+board = Board.new
+board.place_board
